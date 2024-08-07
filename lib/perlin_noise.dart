82dc6d713703175a4b1de17db278fbff61d8d795
suojae3 <suojae3@gmail.com>
2024-08-07 15:15:46 +0900

    chore

--- lib/perlin_noise.dart
+++ lib/perlin_noise.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Perlin Noise Visualization')),
-        body: PerlinNoiseWidget(),
-      ),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text('Perlin Noise Visualization')),
+//         body: PerlinNoiseWidget(),
+//       ),
+//     );
+//   }
+// }
 
 
 class PerlinNoise {
--- lib/random_walker.dart
+++ lib/random_walker.dart
@@ -1,6 +1,6 @@
-// import 'dart:math';
-// import 'dart:ui';
-// import 'package:flutter/material.dart';
+import 'dart:math';
+import 'dart:ui';
+import 'package:flutter/material.dart';
 
 // void main() {
 //   runApp(MyApp());
@@ -18,101 +18,101 @@
 //   }
 // }
 
-// class Walker {
-//   double x;
-//   double y;
-//   final List<int> directions = [1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 3, 4, 4];
-//   final Random rand = Random();
+class Walker {
+  double x;
+  double y;
+  final List<int> directions = [1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 3, 4, 4];
+  final Random rand = Random();
 
-//   Walker(double width, double height)
-//       : x = width / 2,
-//         y = height / 2;
+  Walker(double width, double height)
+      : x = width / 2,
+        y = height / 2;
 
-//   void show(Canvas canvas, Size size) {
-//     final paint = Paint()
-//       ..color = Colors.black
-//       ..strokeWidth = 4;
-//     canvas.drawPoints(PointMode.points, [Offset(x, y)], paint);
-//   }
+  void show(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 4;
+    canvas.drawPoints(PointMode.points, [Offset(x, y)], paint);
+  }
 
-//   void step() {
-//     int choice = directions[rand.nextInt(directions.length)];
-//     bool reverse = rand.nextInt(100) < 1; // 1% 확률로 방향을 반대로
+  void step() {
+    int choice = directions[rand.nextInt(directions.length)];
+    bool reverse = rand.nextInt(100) < 1; // 1% 확률로 방향을 반대로
 
-//     switch (choice) {
-//       case 1:
-//         x += reverse ? -1 : 1;
-//         break;
-//       case 2:
-//         x -= reverse ? -1 : 1;
-//         break;
-//       case 3:
-//         y += reverse ? -1 : 1;
-//         break;
-//       case 4:
-//         y -= reverse ? -1 : 1;
-//         break;
-//     }
-//   }
-// }
+    switch (choice) {
+      case 1:
+        x += reverse ? -1 : 1;
+        break;
+      case 2:
+        x -= reverse ? -1 : 1;
+        break;
+      case 3:
+        y += reverse ? -1 : 1;
+        break;
+      case 4:
+        y -= reverse ? -1 : 1;
+        break;
+    }
+  }
+}
 
-// class RandomWalkWidget extends StatefulWidget {
-//   @override
-//   _RandomWalkWidgetState createState() => _RandomWalkWidgetState();
-// }
+class RandomWalkWidget extends StatefulWidget {
+  @override
+  _RandomWalkWidgetState createState() => _RandomWalkWidgetState();
+}
 
-// class _RandomWalkWidgetState extends State<RandomWalkWidget> {
-//   late Walker walker;
-//   List<Offset> points = [];
+class _RandomWalkWidgetState extends State<RandomWalkWidget> {
+  late Walker walker;
+  List<Offset> points = [];
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     WidgetsBinding.instance.addPostFrameCallback((_) {
-//       final size = MediaQuery.of(context).size;
-//       walker = Walker(size.width, size.height);
-//       _startWalking();
-//     });
-//   }
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final size = MediaQuery.of(context).size;
+      walker = Walker(size.width, size.height);
+      _startWalking();
+    });
+  }
 
-//   void _startWalking() {
-//     Future.doWhile(() async {
-//       setState(() {
-//         walker.step();
-//         points.add(Offset(walker.x, walker.y));
-//       });
-//       await Future.delayed(Duration(microseconds: 1000));
-//       return true;
-//     });
-//   }
+  void _startWalking() {
+    Future.doWhile(() async {
+      setState(() {
+        walker.step();
+        points.add(Offset(walker.x, walker.y));
+      });
+      await Future.delayed(Duration(microseconds: 1000));
+      return true;
+    });
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return CustomPaint(
-//       painter: RandomWalkPainter(points),
-//       child: Container(),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: RandomWalkPainter(points),
+      child: Container(),
+    );
+  }
+}
 
-// class RandomWalkPainter extends CustomPainter {
-//   final List<Offset> points;
+class RandomWalkPainter extends CustomPainter {
+  final List<Offset> points;
 
-//   RandomWalkPainter(this.points);
+  RandomWalkPainter(this.points);
 
-//   @override
-//   void paint(Canvas canvas, Size size) {
-//     final paint = Paint()
-//       ..color = Colors.black
-//       ..strokeWidth = 5;
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 5;
 
-//     for (final point in points) {
-//       canvas.drawPoints(PointMode.points, [point], paint);
-//     }
-//   }
+    for (final point in points) {
+      canvas.drawPoints(PointMode.points, [point], paint);
+    }
+  }
 
-//   @override
-//   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-//     return true;
-//   }
-// }
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
