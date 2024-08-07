fa80a524e6e082ad1b003e6357940a1c1e1426cc
suojae3 <suojae3@gmail.com>
2024-08-07 15:14:49 +0900

    perlin noise

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,127 @@
-import 'dart:math';
-import 'dart:ui';
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+// import 'dart:math';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Random Walk')),
-        body: RandomWalkWidget(),
-      ),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text('Perlin Noise Visualization')),
+//         body: PerlinNoiseWidget(),
+//       ),
+//     );
+//   }
+// }
 
-class Walker {
-  double x;
-  double y;
-  final List<int> directions = [1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 3, 4, 4];
-  final Random rand = Random();
 
-  Walker(double width, double height)
-      : x = width / 2,
-        y = height / 2;
+// class PerlinNoise {
+//   final int seed;
+//   late List<int> permutations;
 
-  void show(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.black
-      ..strokeWidth = 4;
-    canvas.drawPoints(PointMode.points, [Offset(x, y)], paint);
-  }
+//   PerlinNoise({this.seed = 0}) {
+//     _generatePermutations();
+//   }
 
-  void step() {
-    int choice = directions[rand.nextInt(directions.length)];
+//   void _generatePermutations() {
+//     final List<int> tempList = List<int>.generate(256, (i) => i);
+//     final random = Random(seed);
+//     tempList.shuffle(random);
+//     permutations = List<int>.from(tempList)..addAll(tempList);
+//   }
 
-    switch (choice) {
-      case 1:
-        x += 1;
-        break;
-      case 2:
-        x -= 1;
-        break;
-      case 3:
-        y += 1;
-        break;
-      case 4:
-        y -= 1;
-        break;
-    }
-  }
-}
+//   double _fade(double t) {
+//     return t * t * t * (t * (t * 6 - 15) + 10);
+//   }
 
-class RandomWalkWidget extends StatefulWidget {
-  @override
-  _RandomWalkWidgetState createState() => _RandomWalkWidgetState();
-}
+//   double _grad(int hash, double x, double y) {
+//     final h = hash & 15;
+//     final u = h < 8 ? x : y;
+//     final v = h < 4 ? y : (h == 12 || h == 14 ? x : 0);
+//     return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
+//   }
 
-class _RandomWalkWidgetState extends State<RandomWalkWidget> {
-  late Walker walker;
-  List<Offset> points = [];
+//   double _lerp(double t, double a, double b) {
+//     return a + t * (b - a);
+//   }
 
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final size = MediaQuery.of(context).size;
-      walker = Walker(size.width, size.height);
-      _startWalking();
-    });
-  }
+//   double noise(double x, double y) {
+//     final X = x.floor() & 255;
+//     final Y = y.floor() & 255;
 
-  void _startWalking() {
-    Future.doWhile(() async {
-      setState(() {
-        walker.step();
-        points.add(Offset(walker.x, walker.y));
-      });
-      await Future.delayed(Duration(microseconds: 1000));
-      return true;
-    });
-  }
+//     x -= x.floor();
+//     y -= y.floor();
 
-  @override
-  Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: RandomWalkPainter(points),
-      child: Container(),
-    );
-  }
-}
+//     final u = _fade(x);
+//     final v = _fade(y);
 
-class RandomWalkPainter extends CustomPainter {
-  final List<Offset> points;
+//     final aa = permutations[X + permutations[Y]];
+//     final ab = permutations[X + permutations[Y + 1]];
+//     final ba = permutations[X + 1 + permutations[Y]];
+//     final bb = permutations[X + 1 + permutations[Y + 1]];
 
-  RandomWalkPainter(this.points);
+//     final gradAA = _grad(aa, x, y);
+//     final gradBA = _grad(ba, x - 1, y);
+//     final gradAB = _grad(ab, x, y - 1);
+//     final gradBB = _grad(bb, x - 1, y - 1);
 
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.black
-      ..strokeWidth = 5;
+//     final lerpX1 = _lerp(u, gradAA, gradBA);
+//     final lerpX2 = _lerp(u, gradAB, gradBB);
+//     return _lerp(v, lerpX1, lerpX2);
+//   }
+// }
 
-    for (final point in points) {
-      canvas.drawPoints(PointMode.points, [point], paint);
-    }
-  }
+// class PerlinNoisePainter extends CustomPainter {
+//   final PerlinNoise perlinNoise;
 
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
-  }
-}
+//   PerlinNoisePainter(this.perlinNoise);
+
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     final paint = Paint()
+//       ..style = PaintingStyle.fill;
+//     final cellSize = 5.0; // 작은 사각형의 크기
+
+//     for (double y = 0; y < size.height; y += cellSize) {
+//       for (double x = 0; x < size.width; x += cellSize) {
+//         final noiseValue = perlinNoise.noise(x * 0.1, y * 0.1);
+//         final colorValue = ((noiseValue + 1) * 0.5 * 255).toInt();
+//         paint.color = Color.fromARGB(255, colorValue, colorValue, colorValue);
+
+//         canvas.drawRect(Rect.fromLTWH(x, y, cellSize, cellSize), paint);
+//       }
+//     }
+//   }
+
+//   @override
+//   bool shouldRepaint(CustomPainter oldDelegate) {
+//     return true;
+//   }
+// }
+
+// class PerlinNoiseWidget extends StatefulWidget {
+//   @override
+//   _PerlinNoiseWidgetState createState() => _PerlinNoiseWidgetState();
+// }
+
+// class _PerlinNoiseWidgetState extends State<PerlinNoiseWidget> {
+//   late PerlinNoise perlinNoise;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     perlinNoise = PerlinNoise(seed: 0);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return CustomPaint(
+//       painter: PerlinNoisePainter(perlinNoise),
+//       child: Container(),
+//     );
+//   }
+// }
+
